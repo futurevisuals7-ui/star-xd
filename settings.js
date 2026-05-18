928dd973243a3dc55786d53e632ed74c45f19714
futurevisuals7-ui <futurevisuals7@gmail.com>
2026-05-18 03:31:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 export default {
   // PUT YOUR SESSION ID BELOW (Required)
   // Get from: https://star.malvintech.sbs/
-  SESSION_ID: "",
+  SESSION_ID: "king~H4sIAAAAAAAAA5VUyZKjOBD9lQld7SizmcURFTGAF/BubFPYE32QQYDMLgQ27vBxfmi+oz9qAtfSdZjpqeHwQkiQ+fLlS30HaYZLNEMNGHwHOcE1pKhd0iZHYAC0yvcRAV3gQQrBAESzc6TMpar2PG7KKFkZC8zFU5eBtEylqe5PU7uX+RtNZoVncO+CvDrF2P1FwOmslIUVGxtxIu28XSZcjJGHwtV5Uff6cKYhMRRsg5zgZvMM7m1EiAlOg1EeogQRGM9Qs4aYfI1+RyvsZHpR1pPQ5dzghRzkSI7JRGGwjaLrajxci7shOw2cL9LP/MZid9QKpSKiZnTch8I8qF1mE6vuvhHLkK538UhsFPWNfomDFHmmh1KKafNl3YWxcziuXJKI3CIeVYqY6NFUcRFcbGf+3D1fnOOpMbLCGUdfI47l1VKrjWi/6hWwYxb7Xk/oxD2I2MmEvS2kqTM9NPYp4jfMZ+Jr8u6V6P/oPjG4y3KsIIHSJbSyvB6rLIX6OQlOqlaEumwxobhjjPFY/Rr9l0VwnY9vLxdjmPalxqrnwUWaa3XTFzf8LTuvF64dODFrap90h7Qiv2LJr8vSFVTLaKwrScNFNHO3GyRwnsphx0rX+wxPUr4QiriIPdmdDOfGizzaa8xcYMfFrBiGDWsPsQ97HBT6/hKft0IQPD8qilBjemDA3ruAoACXlECKs/Sx1xe6AHr1FrkE0Ye84Cbwnpp62wNkLrVlLb2papO0XuL1jqmjg8ot6HnvQMJV5jPogpxkLipL5Bm4pBlpFqgsYYBKMPjjWxek6EpfG9em49ku8DEp6T6t8jiD3ntX3w+h62ZVSrdN6urtAhEwYH5uI0pxGpStjlUKiRviGukhpCUYUFKhjwIRQd7r1sfQ6pnX6m4L6kSZ7BnQBcmjH9gDA8D1+yKvCAIri/KAE34vny5tVJjnTymioAtS2H4NzB9//viLtlC28NuuxaaFvAXcQtpC8DjWWlx9/PP0BLogfk0oKgzbVxSZYXmR59uc7cH9o9SWmocoxHEJBkBfwnVoqqPRvCMUnjqZqG6g6oEKfkrz7rHXHspLYVdf95j3r+N6dNEnN2+00LJm7NovvTUv05wxO+tJM9Sy538I0rqgF6SCI9TTeXkzAqc6prQ6dyqxSdye6cQdtzwcJWMfcwzfYSi7c0zhcG6Yte3FQyvRNHElBkWHN/Z966hVG+IkaKGqz202D9XYRZ+TDW2WsFlnxfRsPSSd4ag+8pXYualMvpHKbLXbReRwkbRmtOqEfqzyVajvKoVzraQZScn5tDS4oIiEsyPkfJKECtlEZ/XV/Y/pi99uPfzwZdv19tXH6HGJvHX3P13wSrz1KnPvforxdi39y2hr/hCNJNk2KS9bp+tCDUveGqpwETpju74pvTn09PX5WgYXCO73b12Qx5D6GUnAAJTJCYIuIFnVOt9M/ewXmXR1YQ43gd6WHcOSqj+naYcTVFKY5GDASpLCiLwsc12QNGqebymk70MI1PYxTytw/xvzgiKHoAcAAA==",
 
   // PUT YOUR REMOVE BACKGROUND KEY (Optional)
   // Get from: https://remove.bg/api
@@ -39,20 +39,20 @@ export default {
   PREFIXES: ".",
 
   // Bot Pack Name
-  PACKNAME: "sᴛᴀʀ-xᴅ",
+  PACKNAME: "Tech",
 
   // Bot Name
-  BOT_NAME: "sᴛᴀʀ-xᴅ",
+  BOT_NAME: "Tech",
 
   // Add Owner WhatsApp Number (without +)
-  OWNER_NUMBER: "263725787000",
+  OWNER_NUMBER: "255639441868",
 
   // Command Mode: "public" or "private"
   // private = only owner can use commands
   COMMAND_MODE: "private",
 
   // Owner Name
-  BOT_OWNER: "ᴍʀ xᴅᴋɪɴɢ",
+  BOT_OWNER: "Tech",
 
   // Timezone
   TIMEZONE: "Africa/Harare"
